e05407df137783585f90ab2e6159be61d37ed5a1
Saurav Thakur <sauravthakur1341@gmail.com>
2023-12-10 12:54:33 +0545

    feat: logging and common files added

---
+++ src/document_classification/utils/common.py
@@ -0,0 +1,37 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+from document_classification.logging import logger
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            yaml_file_content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded sucessfully.")
+            return ConfigBox(yaml_file_content)
+
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directory(path_to_directory: list, verbose: bool = True) -> None:
+
+    for path in path_to_directory:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"directory created at: {path}")
+
+
+@ensure_annotations
+def get_file_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path))
+    return f"{size_in_kb} KB"
